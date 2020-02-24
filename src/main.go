501781c69ba0532bd3dd2c24f5a7fbc68fea9e15
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-02-24 12:05:22 +0900

    request parameter のバリデーションチェック処理を追加

--- src/main.go
+++ src/main.go
@@ -3,20 +3,20 @@ package main
 import (
 	"net/http"
 	"torimo-article-api/src/handler"
-	"torimo-article-api/src/validate"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"torimo-article-api/src/db"
+	"github.com/go-playground/validator/v10"
 )
 
-// type CustomValidator struct {
-// 	validator *validator.Validate
-// }
+type CustomValidator struct {
+	validator *validator.Validate
+}
 
-// func (cv *CustomValidator) Validate(i interface{}) error {
-// 	return cv.validator.Struct(i)
-// }
+func (cv *CustomValidator) Validate(i interface{}) error {
+	return cv.validator.Struct(i)
+}
 
 func main() {
 
@@ -28,8 +28,7 @@ func main() {
 	e.Use(middleware.Recover())
 
 	// custom validator
-	e.Validator = validate.NewValidator()
-	// e.Validator = &CustomValidator{validator: validator.New()}
+	e.Validator = &CustomValidator{validator: validator.New()}
 
 	// CORS restricted
 	// Allows requests from any `http://localhost:8888` or `https://torimo-a04a5.firebaseapp.com` origin
@@ -44,7 +43,7 @@ func main() {
 	h := handler.NewHandler(d)
 
 	e.POST("/articles", h.CreateArticle)
-	// e.GET("/articles", h.GetArticle)
+	e.GET("/articles", h.GetArticle)
 	// e.GET("/articles/:id", h.GetArticle)
 	// e.PUT("/articles/:id", h.UpdateArticle)
 	// e.DELETE("/articles/:id", h.DeleteArticle)
