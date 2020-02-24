fe97dd3a182a77e910b1b26936baf5457bb212d0
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-02-24 12:04:00 +0900

    記事の新規作成処理を追加

--- src/handler/articles.go
+++ src/handler/articles.go
@@ -1,7 +1,10 @@
 package handler
 
 import (
-	// "torimo-article-api/src/model"
+	"time"
+	"fmt"
+	"torimo-article-api/src/model"
+	// "os"
 	"net/http"
 	"github.com/go-playground/validator/v10"
 
@@ -13,21 +16,12 @@ type RequestArticle struct {
 	Body string            `json:"body"   validate:"required"`
 	Status string          `json:"status" validate:"required,max=16"`
 	UserID int64           `json:"userId" validate:"required"`
-	ShopName string        `json:"shopName"`
-	CategoryNames []string `json:"categoryNames"`
+	ShopID string          `json:"shopId"`
+	CategoryIDs []int      `json:"categoryIds"`
 }
 
 // CreateArticle return error
 func (h *Handler) CreateArticle(c echo.Context) error {
-	// a := &article{
-	// 	ID: seq,
-	// }
-	// if err := c.Bind(a); err != nil {
-	// 	return err
-	// }
-	// articles[a.ID] = a
-	// seq++
-
 	ra := new(RequestArticle)
 
 	if err := c.Bind(ra); err != nil {
@@ -37,13 +31,66 @@ func (h *Handler) CreateArticle(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusBadRequest, err.(validator.ValidationErrors).Error())
 	}
 
-	return c.JSON(http.StatusCreated, ra)
+	// *** request file include. ***
+	// avatar, err := c.FormFile("image")
+  	// if err != nil {
+ 	// 	return err
+	// }
+
+	category := []model.Category{}
+	h.db.Find(&category, "id IN (?)", ra.CategoryIDs)
+	fmt.Println("category ids: ", ra.CategoryIDs)
+	fmt.Println("result: ", category)
+
+	shop := model.Shop{}
+	h.db.Find(&shop, "id=?", ra.ShopID)
+
+	user := model.User{}
+	h.db.Find(&user, "id=?", ra.UserID)
+
+	article := model.Article{
+		Title: ra.Title,
+		Body: ra.Body,
+		Status: ra.Status,
+		User: user,
+		Shop: shop,
+		Categories: category,
+		CreateAt: time.Now(),
+	}
+
+	h.db.Create(&article)
+
+	// TODO: avatar to AWS S3
+	// fmt.Println(avatar)
+
+	return c.JSON(http.StatusCreated, article)
 }
 
 // GetArticle return error
-// func (h *Handler) GetArticle(c echo.Context) error {
-// 	a := model.Article{}
-// 	h.db.Find(&a, "id=?", 1)
+func (h *Handler) GetArticle(c echo.Context) error {
+	article := model.Article{}
+	h.db.Find(&article, "id=?", 1)
 
-// 	return c.JSON(http.StatusOK, a)
-// }
+	// category := []model.Category{}
+	// h.db.Find(&category, "id IN (?)", []int{1, 2})
+
+	// shop := model.Shop{}
+	// h.db.Find(&shop, "id=?", 1)
+
+	// user := model.User{}
+	// h.db.Find(&user, "id=?", 1)
+
+	// a := model.Article{
+	// 	Title: "Jinzhu",
+	// 	Body: "string",
+	// 	Status: "string",
+	// 	User: user,
+	// 	Shop: shop,
+	// 	Categories: []*model.Category{&category},
+	// 	CreateAt: time.Now(),
+	// }
+
+	// h.db.Create(&a)
+
+	return c.JSON(http.StatusOK, article)
+}
