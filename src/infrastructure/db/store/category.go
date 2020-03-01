5bef2a601f193850d55afcfdb2f623a2959c62ad
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-03-01 23:49:43 +0900

    カテゴリーの repository を作成

---
+++ src/infrastructure/db/store/category.go
@@ -0,0 +1,25 @@
+package store
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+
+	"torimo-article-api/src/domain/model"
+	"torimo-article-api/src/domain/repository"
+)
+
+type CategoryDatastore struct {
+	db *gorm.DB
+}
+
+func NewCategoryDatastore(d *gorm.DB) repository.ICategoryRepository {
+	return &CategoryDatastore{
+		db: d,
+	}
+}
+
+func (c *CategoryDatastore) FindByIdIn(ids []uint64) []model.Category {
+	category := []model.Category{}
+	c.db.Find(&category, "id IN (?)", ids)
+	return category
+}
