96f5060e64dc2f6a93fdcedd8d982dbeff828296
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-03-13 22:43:15 +0900

    返り値の型を修正

--- src/usecase/article.go
+++ src/usecase/article.go
@@ -1,10 +1,9 @@
 package usecase
 
 import (
-	"torimo-article-api/src/domain/model"
 	"torimo-article-api/src/handler/request"
 )
 
 type IArticleUsecase interface {
-	Create(requestArticle *request.RequestArticle) model.Article
+	Create(requestArticle *request.RequestArticle) uint64
 }
